87e336e370f0f12704c7eb7a61627e97ba7e14a6
Ana Henriques <pbapohenriques@hotmail.com>
2021-04-11 12:03:42 +0100

    definição do tipo de dados TABLE

--- project_c/includes/sgr.h
+++ project_c/includes/sgr.h
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
+/*!
+* @typedef _GHashTable
+*/
+typedef _GHashTable *TABLE;
+
+/*!
+* @typedef sgr
+*
+* @field   user        Utilizador
+* @field   business    Negócio
+* @field   review      Review
+*/
 typedef struct sgr *SGR;
 
 SGR init_sgr();
--- project_c/src/sgr.c
+++ project_c/src/sgr.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
 #include "dados.h"
+#include "sgr.h"
 
 struct sgr{
 	USER *user;
 	BUSINESS *business;
 	REVIEW *review;
-
 };
+
