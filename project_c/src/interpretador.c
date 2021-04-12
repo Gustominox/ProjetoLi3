a47c0791598d95c4d1b19dae5afbde26e2b63cb2
Ana Henriques <pbapohenriques@hotmail.com>
2021-04-12 09:11:33 +0100

    debug em strsep dos comandos

--- project_c/src/interpretador.c
+++ project_c/src/interpretador.c
@@ -26,6 +26,7 @@ void show(TABLE x){
 /*
 int interpretador(SGR sgr){
     char linha[BUF_SIZE];
+    char **temp = NULL;
     // char col[2], lin[2];
     // char cmdname[BUF_SIZE];
     // char filename[BUF_SIZE];
@@ -36,39 +37,48 @@ int interpretador(SGR sgr){
     if(fgets(linha, BUF_SIZE, stdin) == NULL)
         return ERRO_IO;
 
-    switch(linha){
+    for(int i = 0; linha != NULL; i++){
+        temp = realloc(temp,sizeof(char*)*(i+1));
+        temp[i] = strsep(&temp, ";");
+    }
+    temp[i] = NULL;
 
-        case (strcmp(strsep(&linha, ";"),"query2")==0):
+    int k = 0;
+    while (linha != NULL){
+        
+        switch(linha){
+
+        case (strcmp(strsep(&temp[k], ";"),"query2")==0):
             printf("Qual e a letra com que comeca o nome do negocio?");
             scanf("%c", &letter);
             x = businesses_started_by_letter(sgr, letter);
             // break;
 
-        case (strcmp(strsep(&linha, ";"),"query3")==0):
+        case (strcmp(strsep(&temp[k], ";"),"query3")==0):
             x = business_info(sgr, business_id);
             // break;
         
-        case (strcmp(strsep(&linha, ";"),"query4")==0):
+        case (strcmp(strsep(&temp[k], ";"),"query4")==0):
             x = businesses_reviewed(sgr, user_id);
             // break;
                
-        case (strcmp(strsep(&linha, ";"),"query5")==0):           
+        case (strcmp(strsep(&temp[k], ";"),"query5")==0):           
             x = businesses_with_stars_and_city(sgr, stars, city);
             // break;
         
-        case (strcmp(strsep(&linha, ";"),"query6")==0):
+        case (strcmp(strsep(&temp[k], ";"),"query6")==0):
             x = top_businesses_by_city(sgr, top);
             // break;  
         
-        case (strcmp(strsep(&linha, ";"),"query7")==0):
+        case (strcmp(strsep(&temp[k], ";"),"query7")==0):
             x = international_users(sgr);
             // break;
         
-        case (strcmp(strsep(&linha, ";"),"query8")==0):   
-            x = top_businesses_with_category(sgr, top, categorie);
+        case (strcmp(strsep(&temp[k], ";"),"query8")==0):   
+            x = top_businesses_with_category(sgr, top, categories);
             // break;
         
-        case (strcmp(strsep(&linha, ";"),"query9")==0):
+        case (strcmp(strsep(&temp[k], ";"),"query9")==0):
             x = reviews_with_word(sgr, top, word);
             // break;
         
