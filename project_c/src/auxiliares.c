447718d81ac5c87a7ffc35e6d3937611fcc755b9
Ana Henriques <pbapohenriques@hotmail.com>
2021-04-20 18:21:36 +0100

    função compare() acabada

--- project_c/src/auxiliares.c
+++ project_c/src/auxiliares.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include "auxiliares.h"
 
-int isFloat (const char *s){
+int isFloat (char *s){
   char *str = NULL;
   double flt = strtod (s, &str);
   if (!str  ||  *str)
-      return 0;  // has non-floating digits after number, if any
+      return 0;
   return 1;
 }
 
-int isInteger (const char *s){
+int isInteger (char *s){
   while(*s){
     if(!isDigit(*s)) return 0;   
     s++;
