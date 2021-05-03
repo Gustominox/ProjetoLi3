e2b9ea7b5b7745ba193297200038168773cc7a76
Ana Henriques <pbapohenriques@hotmail.com>
2021-05-03 19:10:46 +0100

    funções auxiliares

--- project_c/includes/auxiliares.h
+++ project_c/includes/auxiliares.h
@@ -17,3 +17,29 @@ int isFloat (char *s);
 * @return 1 se verdadeiro, 0 se falso
 */
 int isInteger (char *s);
+
+
+/**
+ * \brief Função que compara o conteúdo de uma table, numa dada coluna, com o valor de comparação que é dado
+ * @param content conteúdo da table recebida, numa dada coluna
+ * @param value valor de comparação 
+ * @param oper operador de comparação
+ * @return zero se tiver sucesso, 1 caso contrário
+ */ 
+int compare(char* content, char* value, OPERATOR oper);
+
+
+/**
+ * \brief Troca dois arrays de strings
+ * @param xp um array de strings
+ * @param yp outro array de strings
+ */
+void swap(char **xp, char **yp);
+
+
+/**
+ * \brief Ordena uma matriz de forma decrescente segundo o valor de stars de cada linha
+ * @param arr matriz
+ * @param linhas número de linhas
+ */
+void ordenaDecresc(int ***arr, int linhas);
--- project_c/src/auxiliares.c
+++ project_c/src/auxiliares.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include "auxiliares.h"
 
+
 int isFloat (char *s){
   char *str = NULL;
   double flt = strtod (s, &str);
@@ -11,6 +12,7 @@ int isFloat (char *s){
   return 1;
 }
 
+
 int isInteger (char *s){
   while(*s){
     if(!isdigit(*s)) return 0;   
@@ -18,3 +20,96 @@ int isInteger (char *s){
   }
   return 1;
 }
+
+
+int compare(char* content, char* value, OPERATOR oper){
+    int isValid = 1;
+    
+    switch(oper){
+
+        case LT:
+            if(isInteger(value)){
+                if(atoi(content) < atoi(value)) isValid = 0;
+            }
+            else if(isFloat(value)){
+                if(atof(content) < atof(value)) isValid = 0;
+            } else {
+                int k = 0;
+                while(k < strlen(value) && k < strlen(content)){
+                    if(content[k] == value[k])
+                        k++;
+                    else if(content[k] < value[k]){
+                        break;
+                    }
+                    else if(content[k] > value[k]){
+                        isValid = 0;
+                        break;
+                    }
+                }
+            }
+            break;
+        
+        case EQ:
+            if(isInteger(value)){
+                if(atoi(content) == atoi(value)) isValid = 0;
+            }
+            else if(isFloat(value)){
+                if(atof(content) == atof(value)) isValid = 0;
+            } else {
+                int k = 0;
+                while(k < strlen(value) && k < strlen(content)){
+                    if(content[k] == value[k]){
+                        isValid = 0;
+                        k++;
+                    }
+                    else{
+                        isValid = 1;
+                        break;
+                    }
+                }
+            }
+            break;
+
+        case GT:
+            if(isInteger(value)){
+                if(atoi(content) > atoi(value)) isValid = 0;
+            }
+            else if(isFloat(value)){
+                if(atof(content) > atof(value)) isValid = 0;
+            } else {
+                int k = 0;
+                while(k < strlen(value) && k < strlen(content)){
+                    if(content[k] == value[k])
+                        k++;
+                    else if(content[k] > value[k]){
+                        break;
+                    }
+                    else if(content[k] < value[k] || content[k] == '\n'){
+                        isValid = 0;
+                        break;
+                    }
+                }
+            }
+            break;
+    }
+    return isValid;
+}
+
+
+void swap(char **xp, char **yp){
+    char** temp = xp; 
+    xp = yp; 
+    yp = temp; 
+}
+
+
+void ordenaDecresc(int ***arr, int linhas){ 
+    int linhaAtual, linhaPost;
+
+    for (linhaAtual = 0; linhaAtual < linhas - 1; linhaAtual++)
+      for (linhaPost = linhaAtual+1; linhaPost < linhas; linhaPost++)
+
+        if(arr[linhaAtual][2] < arr[linhaPost][2])
+          swap(arr[linhaAtual], arr[linhaPost]);
+}
+
