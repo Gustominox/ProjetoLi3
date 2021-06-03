e635193a9b6292e1bd337d3f75409eaab737e9d1
Anaa10 <a93284@alunos.uminho.pt>
2021-06-03 15:56:41 +0100

    User: metodos gets + sets + construtores + equals + clone + toString

--- project_java/ProjetoJava/User.java
+++ project_java/ProjetoJava/User.java
@@ -9,5 +9,54 @@ public class User
     private String userId;
     private String name;
     //private String friends;
-
+    
+    public User(){
+        this.userId = "";
+        this.name = "";
+    }
+    
+    public User(String user_id, String name){
+        this.userId = userId;
+        this.name = name;
+    }
+    
+    public User(User user){
+        this.userId = user.getUserId();
+        this.name = user.getName();
+    }
+    
+    public String getUserId(){
+        return this.userId;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public User clone(){
+        return new User(this);
+    }
+    
+    public boolean equals(Object obj){
+        if (obj == this) return true;
+        if (obj == null || ! obj.getClass().equals(this.getClass())) return false;
+        User bus = (User) obj;
+        return  this.userId.equals(bus.getUserId()) &&
+            this.name.equals(bus.getName());            
+    }
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("UserId: ").append(this.userId);
+        sb.append("Name: ").append(this.name);
+        return sb.toString();
+    }  
 }
