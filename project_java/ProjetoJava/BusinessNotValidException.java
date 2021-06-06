e6b65f92e55e1cb22411c834c5e834d613abcb2a
Ana Henriques <pbapohenriques@hotmail.com>
2021-06-06 10:57:45 +0100

    classes Exception

---
+++ project_java/ProjetoJava/BusinessNotValidException.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class UserNotValidException here.
+ *
+ * @author grupo 64
+ * @version (a version number or a date)
+ */
+public class BusinessNotValidException extends Exception
+{
+    public BusinessNotValidException(){
+        super();
+    }
+    
+    public BusinessNotValidException(String msg){
+        super(msg);
+    }
+}
