69aa51129c499f9ade9213aad0d63e9d6d30420e
Ana Henriques <pbapohenriques@hotmail.com>
2021-06-16 12:34:47 +0100

    na GestReviews: método que trabalha o ponto 1 da secção Estatísticas

--- project_java/ProjetoJava/model/GestReviews.java
+++ project_java/ProjetoJava/model/GestReviews.java
@@ -23,4 +23,24 @@ public class GestReviews
         fos.flush();
         fos.close();
     }
+    
+    // Nome query1
+    public void principal(String[] infoRev, String[] infoBus, String[] infoUser){
+
+        ManipuladorFich fich = new ManipuladorFich();
+
+        System.out.println("Nome do ficheiro: reviews.csv");
+
+        List<Review> reviewsValidas = auxRev(infoRev);
+
+        // ou List<Review> reviewsValidas = auxRev(infoRev).stream().collect(Collectors.toList());
+
+        System.out.println("Nome do ficheiro: business.csv");
+
+        auxBusiness(infoBus, reviewsValidas);
+
+        System.out.println("Nome do ficheiro: user.csv");
+
+        auxUser(infoUser, reviewsValidas);
+    }
 }
