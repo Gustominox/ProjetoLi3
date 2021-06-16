fd0aedcfc94eeeacc23be8b86b5f29994cd179a0
Gustominox <agstcmps.111@gmail.com>
2021-06-16 21:20:18 +0100

    changes

--- project_java/ProjetoJava/model/Teste.java
+++ project_java/ProjetoJava/model/Teste.java
@@ -11,15 +11,8 @@ import java.io.IOException;
 public class Teste
 {
     public static void main(String args[]) throws IOException {
-        String path1 = "reviews1.csv";
-        String path2 = "business1.csv";
-        String path3 = "users1.csv";
-        
-        String[] infoRev = ManipuladorFich.lerFicheiro(path1);
-        String[] infoBus = ManipuladorFich.lerFicheiro(path2);
-        String[] infoUser = ManipuladorFich.lerFicheiro(path3);
-        
-        GestReviews gRev = new GestReviews();
-        gRev.query1(infoRev, infoBus, infoUser);
+    String path = "reviews1.csv";
+
+    ManipuladorFich.parse(path);
     }
 }
