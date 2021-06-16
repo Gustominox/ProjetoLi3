84ce1473a64cef8e96e5da0519d0ef99492904df
Ana Henriques <pbapohenriques@hotmail.com>
2021-06-16 13:02:43 +0100

    alteração da classe Teste

--- project_java/ProjetoJava/model/Teste.java
+++ project_java/ProjetoJava/model/Teste.java
@@ -11,8 +11,15 @@ import java.io.IOException;
 public class Teste
 {
     public static void main(String args[]) throws IOException {
-        String path = "reviews1.csv";
-
-        //ManipuladorFich.parse(path);
+        String path1 = "ProjetoJava/reviews1.csv";
+        String path2 = "ProjetoJava/business1.csv";
+        String path3 = "ProjetoJava/user1.csv";
+        
+        String[] infoRev = ManipuladorFich.lerFicheiro(path1);
+        String[] infoBus = ManipuladorFich.lerFicheiro(path2);
+        String[] infoUser = ManipuladorFich.lerFicheiro(path3);
+        
+        GestReviews gRev = new GestReviews();
+        gRev.principal(infoRev, infoBus, infoUser);
     }
 }
