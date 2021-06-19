6f3b2738e2e82070809f7957cc61e7b73190b4d3
Gustominox <agstcmps.111@gmail.com>
2021-06-19 20:17:37 +0100

    mudar folder

---
+++ project_java/controller/Controller.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.time.DateTimeException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.AbstractMap.SimpleEntry;
+
+import model.*;
+import view.*;
+
+public class Controller {
+
+    public void menu(){
+        
+    }
+
+ 
+
+}
---
+++ project_java/loaders/Writelog.java
@@ -0,0 +1,45 @@
+package loaders;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+import model.GestReviews;
+import model.Businesses.Business;
+import model.Reviews.Review;
+import model.Users.User;
+public class Writelog {
+    
+
+    public void gravar(GestReviews gest,String nome){
+    
+        try {
+
+        FileOutputStream fileOut = new FileOutputStream(nome + ".dat");
+            for (Business bus : gest.getBus().getList()) {
+                // Creates an ObjectOutputStream
+                ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+                // Writes objects to the output stream
+                objOut.writeObject(bus);
+                objOut.close();
+            }
+            for (Review rev : gest.getRev().getList()) {
+                // Creates an ObjectOutputStream
+                ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+                // Writes objects to the output stream
+                objOut.writeObject(rev);
+                objOut.close();
+            }
+            for (User use : gest.getUser().getList()) {
+                // Creates an ObjectOutputStream
+                ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+                // Writes objects to the output stream
+                objOut.writeObject(use);
+                objOut.close();        
+            }
+        }catch (Exception e) {
+            e.getStackTrace();
+        }
+    
+    }
+
+    
+}
