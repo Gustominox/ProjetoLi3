60c808d8f0a4c250111e58fcc95509b69aadac23
Anaa10 <a93284@alunos.uminho.pt>
2021-06-19 11:50:17 +0100

    Business: documentação e eliminado métodos não usados

--- project_java/ProjetoJava/model/Businesses/Business.java
+++ project_java/ProjetoJava/model/Businesses/Business.java
@@ -1,8 +1,8 @@
 /**
- * Escreva a descrição da classe Business aqui.
+ * Dados sobre um objeto Business.
  * 
- * @author (seu nome) 
- * @version (número de versão ou data)
+ * @author grupo 64 
+ * @version 
  */
 
 package model.Businesses;
@@ -25,6 +25,9 @@ public class Business
     private String state;
     private List<String> categories;
     
+    /**
+     * Construtor por omissão.
+     */
     public Business(){
         this.businessId = "";
         this.name = "";
@@ -33,6 +36,9 @@ public class Business
         this.categories = new ArrayList<String>();
     }
 
+    /**
+     * Contrutor que recebe um business id.
+     */
     public Business(String id){
         this.businessId = id;
         this.name = "";
@@ -41,6 +47,9 @@ public class Business
         this.categories = new ArrayList<String>();
     }
     
+    /**
+     * Construtor parametrizado.
+     */
     public Business(String businessId, String name, String city, String state, List<String> categories){
         this.businessId = businessId;
         this.name = name;
@@ -49,6 +58,9 @@ public class Business
         this.categories = categories.stream().collect(Collectors.toList());
     }
     
+    /**
+     * Construtor de cópia.
+     */
     public Business(Business bus){
         this.businessId = bus.getBusinessId();
         this.name = bus.getName();
@@ -57,65 +69,92 @@ public class Business
         this.categories = bus.getCategories();
     }
     
-    public Business(String[] info){
-        try{
-           addBusiness(info);
-       }
-       catch(BusinessNotValidException e){
-           new Business();
-       }
-   }
-    
+    /**
+    * Método que obtém o business Id do negócio.
+    * @return business Id do negócio
+    */
     public String getBusinessId(){
         return this.businessId;
     }
     
+    /**
+    * Método que obtém o nome do negócio.
+    * @return nome do negócio
+    */
     public String getName(){
         return this.name;
     }
     
+    /**
+    * Método que obtém a cidade do negócio.
+    * @return a cidade do negócio
+    */
     public String getCity(){
         return this.city;
     }
     
+    /**
+    * Método que obtém estado do negócio.
+    * @return estado do negócio
+    */
     public String getState(){
         return this.state;
     }
     
+    /**
+    * Método que obtém as categorias do negócio.
+    * @return categorias do negócio
+    */
     public List<String> getCategories(){
         return this.categories;
     }
     
+    /**
+    * Método que muda o business Id do negócio.
+    * @param businessId do negócio
+    */
     public void setBusinessId(String businessId){
         this.businessId = businessId;
     }
     
+    /**
+    * Método que muda o nome do negócio.
+    * @param name do negócio
+    */
     public void setName(String name){
         this.name = name;
     }
     
+    /**
+    * Método que muda a cidade do negócio.
+    * @param city do negócio
+    */
     public void setCity(String city){
         this.city = city;
     }
     
+    /**
+    * Método que muda o estado do negócio.
+    * @param state do negócio
+    */
     public void setState(String state){
         this.state = state;
     }
     
+    /**
+    * Método que muda as categorias do negócio.
+    * @param categories do negócio
+    */
     public void setCategories(List<String> categories){
         this.categories = categories.stream().collect(Collectors.toList());
     }
 
-    public int nrReviewsTotal(ReviewList reviews){
-        int total = 0;
-
-        for(Review rev: reviews.getList()){
-            if(this.businessId.equals(rev.getBusinessId()))
-                total++;
-        }
-        return total;
-    }
-
+    /**
+     * Método que vai buscar todas as reviews de um negócio.
+     *
+     *@param reviews lista de reviews
+     *@return lista das reviews do negócio
+     */
     public ReviewList getReviews(ReviewList reviews){
         ReviewList reviewsDoBus = new ReviewList();
         List<Review> aux = new ArrayList<>();
@@ -129,51 +168,18 @@ public class Business
     }
     
     /**
-     * Método que constrói um objeto Business, caso todos os campos sejam válidos.
-     */
-    public void addBusiness(String[] linha) throws BusinessNotValidException{
-
-        if(linha[0].length() != 22)
-            throw new BusinessNotValidException(linha[0]);
-        
-        if(linha[1].length() == 0)
-            throw new BusinessNotValidException(linha[1]);
-        
-        if(linha[2].length() == 0)
-            throw new BusinessNotValidException(linha[2]);
-        
-        if(linha[3].length() != 2)
-            throw new BusinessNotValidException(linha[3]);
-        
-        if(linha[4].length() == 0)
-            throw new BusinessNotValidException(linha[4]);
-        
-        setBusinessId(linha[0]);
-        setName(linha[1]);
-        setCity(linha[2]);
-        setState(linha[3]);
-
-        String[] aux = linha[4].split(",");
-        List<String> categorias = new ArrayList<>();
-        for(String s: aux)
-            categorias.add(s);
-        setCategories(categorias);
-    }
-    
-    public static Business parse(String info){
-        String[] camposBus = info.split(";");
-        
-        return new Business(camposBus[0],
-                        camposBus[1],
-                        camposBus[2],
-                        camposBus[3],
-                        Arrays.asList(camposBus[4].split(",")));
-    }
-    
+    * Método que faz um clone de um negócio.
+    * @return o clone
+    */    
     public Business clone(){
         return new Business(this);
     }    
     
+    /**
+     * Método que reescreve o equals de um objeto Business.
+     * @param obj objeto Business
+     * @return verdadeiro se dois objetos Business forem iguais.
+     */
     public boolean equals(Object obj){
         if (obj == this) return true;
         if (obj == null || ! obj.getClass().equals(this.getClass())) return false;
