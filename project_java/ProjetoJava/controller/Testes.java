56e88a6f5206fde1c1e67526739b99097045d6a7
Ana Henriques <pbapohenriques@hotmail.com>
2021-06-19 16:40:19 +0100

    criação da classe Testes para testes de performance

---
+++ project_java/ProjetoJava/controller/Testes.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.io.*;
+import java.lang.*;
+import java.time.DateTimeException;
+import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.stream.*;
+
+import model.*;
+import model.Businesses.*;
+import model.Reviews.*;
+import model.Users.*;
+
+public class Testes{
+
+    
+}
